f39102407b6347b7e96bd7fce56bdfebef55522e
Priyanka <Priyanka@Priyanka-PC>
2020-04-12 00:43:14 +0530

    first commit of velly

---
+++ SeleniumTest/src/test/java/com/demo/selenium/test/FirstTest.java
@@ -0,0 +1,27 @@
+package com.demo.selenium.test;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+  @Test
+  public void f() throws InterruptedException {
+	  Logger log = Logger.getLogger(FirstTest.class);
+	  log.info("*****************Starting test*********************");
+	  
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Priyanka\\Desktop\\Selenium\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  Thread.sleep(5000);
+	  driver.get("https://www.google.co.in");
+	  log.warn("This is warning message....");
+	  
+	  Thread.sleep(5000);
+	  
+	  System.out.println(driver.getTitle());
+	  log.info("****************Ending test***********************");
+	  driver.quit();
+	  
+  }
+}
