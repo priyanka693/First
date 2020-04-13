0a7684b28ee8a646df9dacc3eed3583a49108721
priyanka693 <er.priyanka693@gmail.com>
2020-04-13 17:47:54 +0530

    written new code for read excel.

--- SeleniumTest/src/test/java/Read/Excel/operation/test/readExcel.java
+++ SeleniumTest/src/test/java/Read/Excel/operation/test/readExcel.java
@@ -10,89 +10,80 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import poiOperation.ReadGuru99ExcelFile;
-
 public class readExcel {
-	public void readExcel(String filePath,String fileName,String sheetName) throws IOException{
+	public static void readExcel1() throws IOException {
 
-	    //Create an object of File class to open xlsx file
+		// Create an object of File class to open xlsx file
+		String fileName = "ExportExcel.xlsx";
+		String sheetName = "ExcelGuru99Demo";
 
-	    File file =    new File(filePath+"\\"+fileName);
+		File file = new File("D:\\rough\\First\\SeleniumTest\\src\\excelExportAndFileIO\\ExportExcel.xlsx");
 
-	    //Create an object of FileInputStream class to read excel file
+		// Create an object of FileInputStream class to read excel file
 
-	    FileInputStream inputStream = new FileInputStream(file);
+		FileInputStream inputStream = new FileInputStream(file);
 
-	    Workbook guru99Workbook = null;
+		Workbook guru99Workbook = null;
+		
+		// Find the file extension by splitting file name in substring and
+		// getting only extension name
 
-	    //Find the file extension by splitting file name in substring  and getting only extension name
+		String fileExtensionName = fileName.substring(fileName.indexOf("."));
 
-	    String fileExtensionName = fileName.substring(fileName.indexOf("."));
+		// Check condition if the file is xlsx file
 
-	    //Check condition if the file is xlsx file
+		if (fileExtensionName.equals(".xlsx")) {
 
-	    if(fileExtensionName.equals(".xlsx")){
+			// If it is xlsx file then create object of XSSFWorkbook class
 
-	    //If it is xlsx file then create object of XSSFWorkbook class
+			guru99Workbook = new XSSFWorkbook(inputStream);
 
-	    guru99Workbook = new XSSFWorkbook(inputStream);
+		}
 
-	    }
+		// Check condition if the file is xls file
 
-	    //Check condition if the file is xls file
+		else if (fileExtensionName.equals(".xls")) {
 
-	    else if(fileExtensionName.equals(".xls")){
+			// If it is xls file then create object of HSSFWorkbook class
 
-	        //If it is xls file then create object of HSSFWorkbook class
+			guru99Workbook = new HSSFWorkbook(inputStream);
 
-	        guru99Workbook = new HSSFWorkbook(inputStream);
+		}
 
-	    }
+		// Read sheet inside the workbook by its name
 
-	    //Read sheet inside the workbook by its name
+		Sheet guru99Sheet = guru99Workbook.getSheet(sheetName);
 
-	    Sheet guru99Sheet = guru99Workbook.getSheet(sheetName);
+		// Find number of rows in excel file
 
-	    //Find number of rows in excel file
+		int rowCount = guru99Sheet.getLastRowNum() - guru99Sheet.getFirstRowNum();
 
-	    int rowCount = guru99Sheet.getLastRowNum()-guru99Sheet.getFirstRowNum();
+		// Create a loop over all the rows of excel file to read it
 
-	    //Create a loop over all the rows of excel file to read it
+		for (int i = 0; i < rowCount + 1; i++) {
 
-	    for (int i = 0; i < rowCount+1; i++) {
+			Row row = guru99Sheet.getRow(i);
 
-	        Row row = guru99Sheet.getRow(i);
+			// Create a loop to print cell values in a row
 
-	        //Create a loop to print cell values in a row
+			for (int j = 0; j < row.getLastCellNum(); j++) {
 
-	        for (int j = 0; j < row.getLastCellNum(); j++) {
+				// Print Excel data in console
 
-	            //Print Excel data in console
+				System.out.print(row.getCell(j).getStringCellValue() + "|| ");
 
-	            System.out.print(row.getCell(j).getStringCellValue()+"|| ");
+			}
 
-	        }
+			System.out.println();
+		}
 
-	        System.out.println();
-	    } 
+	}
 
-	    }  
+	// Main function is calling readExcel function to read data from excel file
 
-	    //Main function is calling readExcel function to read data from excel file
+	public static void main(String args[]) throws IOException {
 
-	    public static void main(String...strings) throws IOException{
-
-	    //Create an object of ReadGuru99ExcelFile class
-
-	    ReadGuru99ExcelFile objExcelFile = new ReadGuru99ExcelFile();
-
-	    //Prepare the path of excel file
-
-	    String filePath = System.getProperty("user.dir")+"\\src\\excelExportAndFileIO";
-
-	    //Call read file method of the class to read data
-
-	    objExcelFile.readExcel(filePath,"ExportExcel.xlsx","ExcelGuru99Demo");
-	    }
+		readExcel1();
+	}
 
 }
